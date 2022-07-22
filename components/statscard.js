bb7a3f83c93515357da763f7f326ef151426ee87
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-07-22 17:01:42 +0700

    Pengkondisian warna teks berdasarkan theme di sistem

--- components/statscard.js
+++ components/statscard.js
@@ -3,12 +3,13 @@ import {Stat, StatLabel, StatNumber, useColorModeValue} from "@chakra-ui/react"
 export default function StatsCard(props) {
   return <Stat
     px={{ base: 4, md: 8 }}
-    py={'5'}
-    shadow={'xl'}
-    border={'1px solid'}
-    borderColor={useColorModeValue('gray.800', 'gray.500')}
-    rounded={'lg'}>
-    <StatLabel fontWeight={'medium'}>
+    py={"5"}
+    shadow={"xl"}
+    border={"1px solid"}
+    color={useColorModeValue("gray.800", "gray.500")}
+    borderColor={useColorModeValue("gray.800", "gray.500")}
+    rounded={"lg"}>
+    <StatLabel fontWeight={"medium"}>
       {props.title}
     </StatLabel>
     <StatNumber fontSize={"2xl"} fontWeight={"medium"}>
