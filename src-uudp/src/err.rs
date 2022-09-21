93d7e71941ed67da7294d16dd336049781ee6ce7
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-09-21 17:49:57 +0700

    Tambah error handling baru

--- src-uudp/src/err.rs
+++ src-uudp/src/err.rs
@@ -1,9 +1,10 @@
-use std::{io, string};
+use std::{io, num::ParseFloatError, string};
 
 #[derive(Debug)]
 pub enum AppErr {
     BindSocket(io::Error),
     BytesConversion(string::FromUtf8Error),
+    FloatConversion(ParseFloatError),
 }
 
 impl From<io::Error> for AppErr {
@@ -15,5 +16,11 @@ impl From<io::Error> for AppErr {
 impl From<string::FromUtf8Error> for AppErr {
     fn from(err: string::FromUtf8Error) -> Self {
         AppErr::BytesConversion(err)
+    }
+}
+
+impl From<ParseFloatError> for AppErr {
+    fn from(err: ParseFloatError) -> Self {
+        AppErr::FloatConversion(err)
     }
 }
