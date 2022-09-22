3518062d4bb209bd5d7e6198047ebfbe76d4a167
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-09-22 14:28:26 +0700

    Menjalankan sub command di aplikasi utama

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,21 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+mod err;
+mod handler;
+
+use err::AppErr;
+
+use crate::handler::udp_comm;
+
+fn main() -> Result<(), AppErr> {
+    udp_comm()?;
+
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+
+    Ok(())
 }
