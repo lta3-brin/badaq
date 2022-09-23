91236b1697de9057cd8fb9f4ab30469a367d39ad
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-09-23 07:59:28 +0700

    Update awal plot

--- components/daplot.js
+++ components/daplot.js
@@ -1,21 +1,60 @@
+import { useEffect, useState } from 'react'
 import { Stack, useMantineColorScheme } from '@mantine/core'
 import Plot from "react-plotly.js"
 
-export default function Daplot(props) {
+
+export default function Daplot() {
+    const [dt1, setDt1] = useState([0])
+    const [dt2, setDt2] = useState([0])
+    const [dt3, setDt3] = useState([0])
+    const [dt4, setDt4] = useState([0])
+    const [dt5, setDt5] = useState([0])
+    const [dt6, setDt6] = useState([0])
+
     const { colorScheme } = useMantineColorScheme()
     const dark = colorScheme === "dark"
 
+    useEffect(() => wsInit(), [])
+
+    const wsInit = () => {
+        let socket = new WebSocket("ws://localhost:9001")
+
+        socket.onmessage = function (e) {
+            const data = e.data
+            const payload = data.split(",")
+
+            if (!payload.includes('SEQ') && !payload.includes('ENDSEQ')) {
+                payload.splice(-1)
+
+                setDt1(old => [...old, payload[0]])
+                setDt2(old => [...old, payload[1]])
+                setDt3(old => [...old, payload[2]])
+                setDt4(old => [...old, payload[3]])
+                setDt5(old => [...old, payload[4]])
+                setDt6(old => [...old, payload[5]])
+            }
+        }
+
+        socket.onclose = function (e) {
+            console.log(e)
+        }
+
+        socket.onerror = function (e) {
+            console.log(e)
+        }
+    }
+
     const trace1 = {
         name: 'k1',
-        x: [0, 1, 2],
-        y: [10, 11, 12],
+        y: dt1,
+        xaxis: 'x1',
+        yaxis: 'y1',
         type: 'scatter'
     }
 
     const trace2 = {
         name: 'k2',
-        x: [0, 1, 2],
-        y: [110, 120, 100],
+        y: dt2,
         xaxis: 'x2',
         yaxis: 'y2',
         type: 'scatter'
@@ -23,8 +62,7 @@ export default function Daplot(props) {
 
     const trace3 = {
         name: 'k3',
-        x: [0, 1, 2],
-        y: [1100, 1000, 1200],
+        y: dt3,
         xaxis: 'x3',
         yaxis: 'y3',
         type: 'scatter'
@@ -32,8 +70,7 @@ export default function Daplot(props) {
 
     const trace4 = {
         name: 'k4',
-        x: [0, 1, 2],
-        y: [1200, 1000, 1100],
+        y: dt4,
         xaxis: 'x4',
         yaxis: 'y4',
         type: 'scatter'
@@ -41,8 +78,7 @@ export default function Daplot(props) {
 
     const trace5 = {
         name: 'k5',
-        x: [0, 1, 2],
-        y: [1100, 1000, 1200],
+        y: dt5,
         xaxis: 'x5',
         yaxis: 'y5',
         type: 'scatter'
@@ -50,8 +86,7 @@ export default function Daplot(props) {
 
     const trace6 = {
         name: 'k6',
-        x: [0, 1, 2],
-        y: [1000, 1200, 1100],
+        y: dt6,
         xaxis: 'x6',
         yaxis: 'y6',
         type: 'scatter'
@@ -63,7 +98,7 @@ export default function Daplot(props) {
         style={{ padding: 8 }}
     >
         <Plot
-            data={[trace1, [], trace2, trace3, trace4, trace5, trace6]}
+            data={[trace1, trace2, trace3, trace4, trace5, trace6]}
             layout={{
                 height: 560,
                 paper_bgcolor: dark ? "#25262B" : "#DEE2E6",
