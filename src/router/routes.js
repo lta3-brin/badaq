56321c03959c879fe41b958e4e749c525d694aab
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-09-24 14:06:16 +0700

    Koreksi import page dan component

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/Default.vue'),
+    component: () => import('layouts/DefaultLayout.vue'),
     children: [
       { path: '', component: () => import('pages/index/IndexPage.vue') }
     ]
