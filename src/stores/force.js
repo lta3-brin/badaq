c0b341a3ef8f56218c72ab4f161c5a9a369caa1f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-09-24 23:21:08 +0700

    Modifikasi state lanjut

--- src/stores/force.js
+++ src/stores/force.js
@@ -2,11 +2,18 @@ import { defineStore } from 'pinia'
 
 export const useForceStore = defineStore('force', {
   state: () => ({
-    k1: {SEQ1: [0, 10, 5, 2, 20, 30, 45]},
-    k2: {SEQ1: []},
-    k3: {SEQ1: []},
-    k4: {SEQ1: []},
-    k5: {SEQ1: []},
-    k6: {SEQ1: []},
+    chart: null,
+    k1: {"SEQ01": [0]},
+    k2: {"SEQ01": [0]},
+    k3: {"SEQ01": [0]},
+    k4: {"SEQ01": [0]},
+    k5: {"SEQ01": [0]},
+    k6: {"SEQ01": [0]},
+    p1: [],
+    p2: [],
+    p3: [],
+    p4: [],
+    p5: [],
+    p6: [],
   })
 })
