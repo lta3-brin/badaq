cb14f12ea4539e509a8765a49d4d28ac99316a2f
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-09-25 19:40:32 +0700

    Membuat error handling untuk csv error

--- src-uudp/src/err.rs
+++ src-uudp/src/err.rs
@@ -1,10 +1,12 @@
 use std::{io, num::ParseFloatError, string};
+use csv;
 
 #[derive(Debug)]
 pub enum AppErr {
     BindSocket(io::Error),
     BytesConversion(string::FromUtf8Error),
     FloatConversion(ParseFloatError),
+    CsvError(csv::Error)
 }
 
 impl From<io::Error> for AppErr {
@@ -22,5 +24,11 @@ impl From<string::FromUtf8Error> for AppErr {
 impl From<ParseFloatError> for AppErr {
     fn from(err: ParseFloatError) -> Self {
         AppErr::FloatConversion(err)
+    }
+}
+
+impl From<csv::Error> for AppErr {
+    fn from(err: csv::Error) -> Self {
+        AppErr::CsvError(err)
     }
 }
