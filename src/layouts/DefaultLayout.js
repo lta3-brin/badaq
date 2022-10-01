762702290e0ad55f3313f926b79fe4c07daeb60a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-10-01 11:21:52 +0700

    Menambahkan label koneksi

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,10 +1,18 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import { useForceStore } from 'src/stores/force'
 
 export default defineComponent({
   name: 'MainLayout',
   setup () {
+    const store = useForceStore()
+    const showDialog = ref(false)
+    const isConnected = computed(() => {
+      return store.connected
+    })
+
     return {
-      showDialog: ref(false)
+      showDialog,
+      isConnected
     }
   }
 })
