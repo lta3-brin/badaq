5cc7d2cf8efeb200cd8281da096f91242b857516
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-10-01 17:40:26 +0700

    Koreksi nama sidecar yang digunakan

--- src-tauri/src/handler.rs
+++ src-tauri/src/handler.rs
@@ -3,7 +3,7 @@ use tauri::api::process::Command;
 use crate::err::AppErr;
 
 pub fn udp_comm() -> Result<(), AppErr> {
-    let tt = Command::new_sidecar("src-uudp")?;
+    let tt = Command::new_sidecar("uudp")?;
 
     tt.spawn()?;
 
