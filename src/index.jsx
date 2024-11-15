1e50d93c08b6834fcfbe82f0f9d9ecb94b15c357
AKBARPN136 <akbarpn136@gmail.com>
2024-11-15 10:09:43 +0700

    Mendaftarkan state

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,14 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import App from "./App";
 
-render(() => <App />, document.getElementById("root"));
+import App from "./App";
+import { AppContext, setState, state } from "./stores";
+
+render(
+  () => (
+    <AppContext.Provider value={{ state, setState }}>
+      <App />
+    </AppContext.Provider>
+  ),
+  document.getElementById("root")
+);
