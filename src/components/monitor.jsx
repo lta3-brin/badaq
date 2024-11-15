2b35d6c484e40f07ff63fa6e0e7fbc06092bb412
AKBARPN136 <akbarpn136@gmail.com>
2024-11-15 10:10:05 +0700

    Menerapkan state kedalam plot

--- src/components/monitor.jsx
+++ src/components/monitor.jsx
@@ -1,16 +1,10 @@
-import { onMount } from "solid-js";
+import { onMount, useContext } from "solid-js";
 import Plotly from "plotly.js-dist-min";
 
 import DefaultCard from "./card";
+import { AppContext } from "../stores";
 
 export default function DefaultMonitor() {
-  let k1;
-  let k2;
-  let k3;
-  let k4;
-  let k5;
-  let k6;
-
   const layout = {
     responsive: true,
     showlegend: true,
@@ -39,6 +33,8 @@ export default function DefaultMonitor() {
     displaylogo: false,
   };
 
+  const { state, setState } = useContext(AppContext);
+
   onMount(() => {
     const trace1_k1 = {
       x: [0, 1, 2, 3, 4],
@@ -54,27 +50,27 @@ export default function DefaultMonitor() {
       name: "polar 2",
     };
 
-    Plotly.newPlot(k1, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(k2, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(k3, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(k4, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(k5, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(k6, [trace1_k1, trace2_k1], layout, configs);
+    Plotly.newPlot(state.k1, [trace1_k1, trace2_k1], layout, configs);
+    Plotly.newPlot(state.k2, [trace1_k1, trace2_k1], layout, configs);
+    Plotly.newPlot(state.k3, [trace1_k1, trace2_k1], layout, configs);
+    Plotly.newPlot(state.k4, [trace1_k1, trace2_k1], layout, configs);
+    Plotly.newPlot(state.k5, [trace1_k1, trace2_k1], layout, configs);
+    Plotly.newPlot(state.k6, [trace1_k1, trace2_k1], layout, configs);
   });
 
   return (
     <div class="container mx-auto px-4 pt-4 space-y-8">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
         <DefaultCard title="Force on x axis" description="X = DSN, Y = Fx (N)">
-          <div ref={k1} class="w-full" />
+          <div ref={(el) => setState("k1", el)} class="w-full" />
         </DefaultCard>
 
         <DefaultCard title="Force on y axis" description="X = DSN, Y = Fy (N)">
-          <div ref={k2} class="w-full" />
+          <div ref={(el) => setState("k2", el)} class="w-full" />
         </DefaultCard>
 
         <DefaultCard title="Force on z axis" description="X = DSN, Y = Fz (N)">
-          <div ref={k3} class="w-full" />
+          <div ref={(el) => setState("k3", el)} class="w-full" />
         </DefaultCard>
       </div>
 
@@ -83,21 +79,21 @@ export default function DefaultMonitor() {
           title="Moment on x axis"
           description="X = DSN, Y = Mx (Nm)"
         >
-          <div ref={k4} class="w-full" />
+          <div ref={(el) => setState("k4", el)} class="w-full" />
         </DefaultCard>
 
         <DefaultCard
           title="Moment on y axis"
           description="X = DSN, Y = My (Nm)"
         >
-          <div ref={k5} class="w-full" />
+          <div ref={(el) => setState("k5", el)} class="w-full" />
         </DefaultCard>
 
         <DefaultCard
           title="Moment on z axis"
           description="X = DSN, Y = Mz (Nm)"
         >
-          <div ref={k6} class="w-full" />
+          <div ref={(el) => setState("k6", el)} class="w-full" />
         </DefaultCard>
       </div>
     </div>
