68aca9c459f5742818ba9e54ee326ecddf0eb468
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-11-20 11:43:36 +0700

    Membuat modul deploy untuk aerolib

---
+++ src-tauri/aerolib/src/aerotauri/deploy.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use std::sync::{Arc, Mutex};
+use tauri::{App, Manager};
+use tauri_plugin_shell::ShellExt;
+
+pub fn deploy_surreal(app: &mut App) -> Result<()> {
+    let pwd = std::env::var("SURREAL_PWD")?;
+
+    let surreal_cmd = app.shell().sidecar("surreal")?.args([
+        "start",
+        "--user",
+        "root",
+        "--pass",
+        &pwd,
+        "rocksdb:badaq.db",
+    ]);
+
+    log::info!("{}", "Spawning database...");
+    let (_rx, comm) = surreal_cmd.spawn()?;
+    let cmd = Arc::new(Mutex::new(Some(comm)));
+    let cmdclone = cmd.clone();
+
+    if let Some(window) = app.get_webview_window("main") {
+        window.on_window_event(move |event| match event {
+            tauri::WindowEvent::Destroyed => match cmdclone.lock() {
+                Ok(mut cmdlock) => {
+                    if let Some(cmdclone_lock) = cmdlock.take() {
+                        if let Err(err) = cmdclone_lock.kill() {
+                            log::error!("{}", err.to_string());
+                        }
+                    }
+                }
+
+                Err(err) => log::error!("{}", err.to_string()),
+            },
+
+            _ => {}
+        });
+    }
+
+    Ok(())
+}
