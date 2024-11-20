ed4e0e355d5b9b54cfc3daefd8fb7c715f78b7e1
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-11-20 11:44:58 +0700

    Hapus modul-modul yang tidak digunakan

--- src-dummy/src/main.rs
+++
@@ -1,9 +0,0 @@
-use aerolib::aeronet::net;
-use anyhow::Result;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    net::try_connect("127.0.0.1:20508").await?;
-
-    Ok(())
-}
--- src-tauri/src/utils/env.rs
+++
@@ -1,9 +0,0 @@
-use anyhow::Result;
-use tauri::{path::BaseDirectory, App, Manager};
-
-pub fn load_env(app: &mut App) -> Result<()> {
-    let envpth = app.path().resolve(".env", BaseDirectory::Resource)?;
-    dotenv::from_path(envpth)?;
-
-    Ok(())
-}
