3d1ae4672c7fa1c52048f35662e88f2d122876cb
AKBARPN136 <akbarpn136@gmail.com>
2024-11-21 12:41:21 +0700

    Modifikasi lanjut koneksi tcp klien

--- src-tauri/src/cmd/net.rs
+++ src-tauri/src/cmd/net.rs
@@ -1,26 +1,49 @@
-use anyhow::Result;
+use core::str;
+use std::io;
+
+use aerolib::aerotauri::tcp::TcpKlien;
 
 #[tauri::command]
-pub async fn try_connect(addr: String, reader: tauri::ipc::Channel<String>) -> Result<()> {
-    let stream = TcpStream::connect(addr).await?;
+pub async fn try_connect(addr: String, onevent: tauri::ipc::Channel<String>) {
+    let klien = TcpKlien::new(addr);
 
-    loop {
-        stream.readable().await?;
-        let mut buf = vec![0; 1024];
+    match klien.get_stream().await {
+        Ok(stream) => loop {
+            match stream.readable().await {
+                Ok(_) => {
+                    let mut buf = vec![0; 1024];
 
-        match stream.try_read(&mut buf) {
-            Ok(0) => break,
-            Ok(_) => {
-                println!("{}", str::from_utf8(&buf)?);
+                    match stream.try_read(&mut buf) {
+                        Ok(0) => {
+                            onevent.send(format!("LOST")).unwrap();
+
+                            break;
+                        }
+
+                        Ok(_) => match str::from_utf8(&buf) {
+                            Ok(message) => {
+                                onevent.send(message.to_string()).unwrap();
+
+                                println!("{}", message);
+                            }
+
+                            Err(err) => {
+                                onevent.send(format!("ERROR: {}", err.to_string())).unwrap()
+                            }
+                        },
+
+                        Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
+                            continue;
+                        }
+
+                        Err(err) => onevent.send(format!("ERROR: {}", err.to_string())).unwrap(),
+                    }
+                }
+
+                Err(err) => onevent.send(format!("ERROR: {}", err.to_string())).unwrap(),
             }
+        },
 
-            Err(ref err) if err.kind() == io::ErrorKind::WouldBlock => {
-                continue;
-            }
-
-            Err(err) => log::error!("{}", err.to_string()),
-        }
+        Err(err) => onevent.send(format!("ERROR: {}", err.to_string())).unwrap(),
     }
-
-    Ok(())
 }
