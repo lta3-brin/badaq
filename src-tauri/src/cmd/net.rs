668b041f40dee27547618c74523eec094a34eb40
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-11-29 17:20:00 +0700

    Ekstraksi nilai dalam dataframe

--- src-tauri/src/cmd/net.rs
+++ src-tauri/src/cmd/net.rs
@@ -51,18 +51,29 @@ pub async fn try_connect(app: AppHandle, addr: String, onevent: tauri::ipc::Chan
 
                                     match klien.parse_buff(message.trim().to_string()) {
                                         Ok(lf) => {
-                                            let nlf = klien
-                                                .calc_data(
-                                                    lf,
-                                                    state.corr.clone(),
-                                                    state.seq.clone(),
-                                                    dsn,
-                                                )
+                                            let df = klien
+                                                .calc_data(lf, state.corr.clone())
                                                 .unwrap()
                                                 .collect()
                                                 .unwrap();
 
-                                            println!("{}", nlf);
+                                            let mut payload =
+                                                format!("{},{},", state.seq.clone(), dsn);
+                                            let koloms = vec![
+                                                "k1", "k1_mean", "k1_rms", "k1_std", "k2",
+                                                "k2_mean", "k2_rms", "k2_std", "k3", "k3_mean",
+                                                "k3_rms", "k3_std", "k4", "k4_mean", "k4_rms",
+                                                "k4_std", "k5", "k5_mean", "k5_rms", "k5_std",
+                                                "k6", "k6_mean", "k6_rms", "k6_std",
+                                            ];
+
+                                            for kol in df.columns(koloms).unwrap() {
+                                                if let Some(val) = kol.f64().unwrap().get(0) {
+                                                    payload.push_str(&format!("{},", val))
+                                                }
+                                            }
+
+                                            onevent.send(payload).unwrap();
                                         }
 
                                         Err(err) => onevent
