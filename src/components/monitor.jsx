51d738bbae3a70546c0e5081baf00ac930e3640d
AKBARPN136 <akbarpn136@gmail.com>
2024-12-05 15:03:09 +0700

    Modifikasi lanjut interaksi plot

--- src/components/monitor.jsx
+++ src/components/monitor.jsx
@@ -1,21 +1,31 @@
 import {
+  createEffect,
   createResource,
   createSignal,
   onMount,
   Suspense,
   useContext,
 } from "solid-js";
-import { invoke, Channel } from "@tauri-apps/api/core";
+import { Channel, invoke } from "@tauri-apps/api/core";
 import Plotly from "plotly.js-dist-min";
 
 import DefaultCard from "./card";
 import { AppContext } from "../stores";
 import { SimpleDialog } from "./dialog";
+import { createStore, produce } from "solid-js/store";
 
 export default function DefaultMonitor() {
   let refSimpleDialog;
-  const { state, setState } = useContext(AppContext);
   const [message, setMessage] = createSignal();
+  const { state, setState } = useContext(AppContext);
+  const [store, setStore] = createStore({
+    data1: [],
+    data2: [],
+    data3: [],
+    data4: [],
+    data5: [],
+    data6: [],
+  });
 
   const layout = {
     responsive: true,
@@ -62,7 +72,180 @@ export default function DefaultMonitor() {
         dialog.showModal();
       } else {
         dialog.close();
-        console.log(msg.split(","));
+
+        if (msg.includes("SEQ")) {
+          const payload = msg.split(",");
+
+          if (payload.length === 1 && !msg.includes("ENDSEQ")) {
+            setStore("data1", store.data1.length, {
+              unik: `k1_${payload[0]}`,
+              type: "scatter",
+              mode: "lines",
+              name: payload[0],
+              x: [],
+              y: [],
+              error_y: {
+                type: "data",
+                array: [],
+                visible: true,
+              },
+            });
+
+            setStore("data2", store.data2.length, {
+              unik: `k2_${payload[0]}`,
+              type: "scatter",
+              mode: "lines",
+              name: payload[0],
+              x: [],
+              y: [],
+              error_y: {
+                type: "data",
+                array: [],
+                visible: true,
+              },
+            });
+
+            setStore("data3", store.data3.length, {
+              unik: `k3_${payload[0]}`,
+              type: "scatter",
+              mode: "lines",
+              name: payload[0],
+              x: [],
+              y: [],
+              error_y: {
+                type: "data",
+                array: [],
+                visible: true,
+              },
+            });
+
+            setStore("data4", store.data4.length, {
+              unik: `k4_${payload[0]}`,
+              type: "scatter",
+              mode: "lines",
+              name: payload[0],
+              x: [],
+              y: [],
+              error_y: {
+                type: "data",
+                array: [],
+                visible: true,
+              },
+            });
+
+            setStore("data5", store.data5.length, {
+              unik: `k5_${payload[0]}`,
+              type: "scatter",
+              mode: "lines",
+              name: payload[0],
+              x: [],
+              y: [],
+              error_y: {
+                type: "data",
+                array: [],
+                visible: true,
+              },
+            });
+
+            setStore("data6", store.data6.length, {
+              unik: `k6_${payload[0]}`,
+              type: "scatter",
+              mode: "lines",
+              name: payload[0],
+              x: [],
+              y: [],
+              error_y: {
+                type: "data",
+                array: [],
+                visible: true,
+              },
+            });
+          } else {
+            setStore(
+              "data1",
+              (el) => el.unik === `k1_${payload[0]}`,
+              produce((el) => {
+                const x = parseInt(payload[1].split("-")[1]);
+                const y = parseFloat(payload[3]);
+                const y_std = parseFloat(payload[5]);
+
+                el.x.push(x);
+                el.y.push(y);
+                el.error_y.array.push(y_std);
+              }),
+            );
+
+            setStore(
+              "data2",
+              (el) => el.unik === `k2_${payload[0]}`,
+              produce((el) => {
+                const x = parseInt(payload[1].split("-")[1]);
+                const y = parseFloat(payload[7]);
+                const y_std = parseFloat(payload[9]);
+
+                el.x.push(x);
+                el.y.push(y);
+                el.error_y.array.push(y_std);
+              }),
+            );
+
+            setStore(
+              "data3",
+              (el) => el.unik === `k3_${payload[0]}`,
+              produce((el) => {
+                const x = parseInt(payload[1].split("-")[1]);
+                const y = parseFloat(payload[11]);
+                const y_std = parseFloat(payload[13]);
+
+                el.x.push(x);
+                el.y.push(y);
+                el.error_y.array.push(y_std);
+              }),
+            );
+
+            setStore(
+              "data4",
+              (el) => el.unik === `k4_${payload[0]}`,
+              produce((el) => {
+                const x = parseInt(payload[1].split("-")[1]);
+                const y = parseFloat(payload[15]);
+                const y_std = parseFloat(payload[17]);
+
+                el.x.push(x);
+                el.y.push(y);
+                el.error_y.array.push(y_std);
+              }),
+            );
+
+            setStore(
+              "data5",
+              (el) => el.unik === `k5_${payload[0]}`,
+              produce((el) => {
+                const x = parseInt(payload[1].split("-")[1]);
+                const y = parseFloat(payload[19]);
+                const y_std = parseFloat(payload[21]);
+
+                el.x.push(x);
+                el.y.push(y);
+                el.error_y.array.push(y_std);
+              }),
+            );
+
+            setStore(
+              "data6",
+              (el) => el.unik === `k6_${payload[0]}`,
+              produce((el) => {
+                const x = parseInt(payload[1].split("-")[1]);
+                const y = parseFloat(payload[23]);
+                const y_std = parseFloat(payload[25]);
+
+                el.x.push(x);
+                el.y.push(y);
+                el.error_y.array.push(y_std);
+              }),
+            );
+          }
+        }
       }
     };
 
@@ -74,31 +257,19 @@ export default function DefaultMonitor() {
 
   const [_, { refetch }] = createResource(fetchTcp);
 
-  onMount(async () => {
+  onMount(() => {
     refSimpleDialog.addEventListener("click", () => {
       refetch();
     });
+  });
 
-    const trace1_k1 = {
-      x: [0, 1, 2, 3, 4],
-      y: [0, 10, 15, 13, 17],
-      type: "scatter",
-      name: "polar 1",
-    };
-
-    const trace2_k1 = {
-      x: [1, 2, 3, 4],
-      y: [16, 5, 11, 9],
-      type: "scatter",
-      name: "polar 2",
-    };
-
-    Plotly.newPlot(state.k1, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(state.k2, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(state.k3, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(state.k4, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(state.k5, [trace1_k1, trace2_k1], layout, configs);
-    Plotly.newPlot(state.k6, [trace1_k1, trace2_k1], layout, configs);
+  createEffect(async () => {
+    Plotly.newPlot(state.k1, store.data1, layout, configs);
+    Plotly.newPlot(state.k2, store.data2, layout, configs);
+    Plotly.newPlot(state.k3, store.data3, layout, configs);
+    Plotly.newPlot(state.k4, store.data4, layout, configs);
+    Plotly.newPlot(state.k5, store.data5, layout, configs);
+    Plotly.newPlot(state.k6, store.data6, layout, configs);
   });
 
   return (
