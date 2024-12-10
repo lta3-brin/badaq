ab6015e480b5e82e79245e20e8e2808dd2021be5
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-12-10 22:27:44 +0700

    Tambah key baru untuk state

--- src-tauri/aerolib/src/aerotauri/state.rs
+++ src-tauri/aerolib/src/aerotauri/state.rs
@@ -3,6 +3,7 @@ use polars::prelude::LazyFrame;
 #[derive(Default)]
 pub struct AppState {
     pub seq: String,
+    pub dsn: String,
     pub nama: String,
     pub corr: LazyFrame,
     pub coef_calib: LazyFrame,
