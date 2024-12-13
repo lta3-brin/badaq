4459beb08ddad5a48739d102b882285d8e2e056b
Arizal Akbar Zikri <akbarpn136@gmail.com>
2024-12-13 18:27:07 +0700

    Mendaftarkan plugin storage

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -10,6 +10,7 @@ use cmd::net::try_connect;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(
